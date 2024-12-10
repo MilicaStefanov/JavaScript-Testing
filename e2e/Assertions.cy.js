c8bc27c66500fe3d763e620516c775ac2a05d3cb
Milica Stefanov <milicastefanov97@yahoo.com>
2024-12-10 21:41:12 +0200

    Update Assertions.cy.js

--- e2e/Assertions.cy.js
+++ e2e/Assertions.cy.js
@@ -4,11 +4,67 @@ describe("Assertions demo", () => {
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
 
-    cy.url().should("include", "orangehrmlive.com");
-    cy.url().should(
-      "eq",
+    // cy.url().should("include", "orangehrmlive.com");
+    // cy.url().should(
+    //   "eq",
+    //   "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    // );
+    // cy.url().should("contain", "orangehrm");
+
+    // cy.url()
+    //   .should("include", "orangehrmlive.com")
+    //   .should(
+    //     "eq",
+    //     "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    //   )
+    //   .should("contain", "orangehrm");
+
+    cy.url()
+      .should("include", "orangehrmlive.com")
+      .and(
+        "eq",
+        "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+      )
+      .and("contain", "orangehrm")
+      .and("not.contain", "greenhrm");
+
+    cy.title()
+      .should("include", "Orange")
+      .and("eq", "OrangeHRM")
+      .and("contain", "HRM");
+
+    cy.get(".orangehrm-login-branding > img")
+      .should("be.visible") // check if the logo is visible
+      .and("exist"); // check if the logo exist
+
+    cy.xpath("//a").should("have.length", "5"); // check all of the links
+
+    cy.get('[name="username"]').type("Admin"); //provide a value into input box
+
+    cy.get('[name="username"]').should("have.value", "Admin");
+  });
+
+  it("Explicite asscetions", () => {
+    cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.url().should("contain", "orangehrm");
+
+    cy.get('[name="username"]').type("Admin");
+    cy.get('[name="password"]').type("admin123");
+    cy.get("button[type='submit']").click();
+
+    let expName = "John Test";
+
+    cy.get(".oxd-userdropdown-name").then((x) => {
+      let actName = x.text();
+
+      //BDD Style - Behavior-Driven Development
+      expect(actName).to.equal(expName);
+      expect(actName).to.not.equal(expName);
+
+      //TDD Style - Test-Driven Development
+      assert.equal(actName, expName);
+      assert.not.equal(actName, expName);
+    });
   });
 });
